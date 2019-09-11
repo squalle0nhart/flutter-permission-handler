e68cf165b897932aed2d79c6f8df501a49d1adbb
squalle0nhart <20115754.bka@gmail.com>
2019-09-11 16:19:39 +0700

    trip out all unused permission

--- ios/Classes/PermissionHandlerEnums.h
+++ ios/Classes/PermissionHandlerEnums.h
@@ -6,23 +6,7 @@
 //
 
 typedef NS_ENUM(int, PermissionGroup) {
-    PermissionGroupCalendar = 0,
     PermissionGroupCamera,
-    PermissionGroupContacts,
-    PermissionGroupLocation,
-    PermissionGroupLocationAlways,
-    PermissionGroupLocationWhenInUse,
-    PermissionGroupMediaLibrary,
-    PermissionGroupMicrophone,
-    PermissionGroupPhone,
-    PermissionGroupPhotos,
-    PermissionGroupReminders,
-    PermissionGroupSensors,
-    PermissionGroupSms,
-    PermissionGroupSpeech,
-    PermissionGroupStorage,
-    PermissionGroupIgnoreBatteryOptimizations,
-    PermissionGroupUnknown,
 };
 
 typedef NS_ENUM(int, PermissionStatus) {
